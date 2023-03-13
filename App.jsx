5c0876782c184b227bbf891462ff3a734b08fb17
Bhatsharvari <123368186+Bhatsharvari@users.noreply.github.com>
2023-03-13 21:52:45 +0530

    Add files via upload
    Added

--- App.jsx
+++ App.jsx
@@ -1,24 +1,59 @@
-//
+//by using constructor()
+
+// import React from "react"
+// import { Component } from "react"
+// // import App from './App';
+
+// class App extends Component{
+    
+//     constructor(){
+        
+//         super()
+//         console.log(this);
+//             this.state={
+//                 username:"prashika"
+                 
+//         }
+//     }
+//     render(){
+//         return(
+            
+//         <>
+//              <h1 style={{color:"purple"}}>{this.state.username}</h1>
+
+//         </>
+//         )
+//     }
+// }
+// export default App
+
+//by using state object
 
 import React from "react"
-import { Component } from "react"
-// import App from './App';
+import { Component } from 'react';
 
 class App extends Component{
-    constructor(){
-        super()
-            this.state={
-                username:"prashika"
-                 
-        }
+
+    state={
+    
+        username:"Asha",
+        // password:"Ashu2020"
+        skills:["java","python","JS"]
     }
     render(){
+        console.log(this);
         return(
         <>
-             <h1 style={{color:"purple"}}>{this.state.username}</h1>
-
-        </>
+        <h2>{this.state.username}</h2>
+        <ul>
+           {this.state.skills.map((x)=>{
+            return <li>{x}</li>
+           })}
+        </ul>
+        {/* <h3>{this.state.password}</h3> */}
+        </> 
         )
     }
+
 }
 export default App
